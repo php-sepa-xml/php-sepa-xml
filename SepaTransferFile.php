2ce554a3e8daad7bd3d752c6bfa68de4bf6f66b9
Ianaré Sévi <ianare@gmail.com>
2012-10-22 14:14:55 +0200

    remove params array, use object properties

--- SepaTransferFile.php
+++ SepaTransferFile.php
@@ -2,10 +2,10 @@
 
 /**
  * Generate a SEPA transfer file.
- * 
+ *
  * ALPHA QUALITY SOFTWARE
  * Do NOT use in production environments!!!
- * 
+ *
  * @copyright © Digitick <www.digitick.net> 2012
  * @license GNU Lesser General Public License v3.0
  * @author Jérémy Cambon
@@ -14,7 +14,7 @@
 
 /**
  * SEPA file object.
- * 
+ *
  * TODO: inherit from PHP XML object?
  */
 class SepaTransferFile
@@ -25,18 +25,19 @@ class SepaTransferFile
 	public $debtorAgentBIC;
 	public $initiatingPartyName;
 	public $paymentInfoId;
-	
+
 	public $headerControlSum = 0;
 	public $paymentControlSum = 0;
-	
+
 	public $paymentMethod = 'TRF';
 	public $paymentTypeInfoCode = 'SEPA';
 	public $chargeBearer = 'SLEV';
 	public $debtorAccountCurrency = 'EUR';
-	
+
 	protected $creditorList = array();
 	protected $numberOfTransactions = 0;
 
+	const INITIAL_STRING = '<?xml version="1.0" encoding="UTF-8"?><Document xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="urn:iso:std:iso:20022:tech:xsd:pain.001.001.03"></Document>';
 
 	public function addCreditor(array $creditor)
 	{
@@ -45,66 +46,47 @@ class SepaTransferFile
 		$this->numberOfTransactions++;
 	}
 
-	public function generateXml(array $data)
+	public function generateXml()
 	{
-		$datetime = new DateTime();		
-		$params = array(
-			'CreationDateTime' => $datetime->format('Y-m-d\TH:i:s'),
-			'HeaderControlSum' => $this->headerControlSum,
-			'InitiatingPartyName' => $this->initiatingPartyName,
-			'PaymentInfoId' => $this->paymentInfoId,
-			'PaymentMethod' => $this->paymentMethod,
-			'PaymentControlSum' => $this->paymentControlSum,
-			'PaymentTypeInfoCode' => $this->paymentTypeInfoCode,
-			'RequestedExecutionDate' => $datetime->format('Y-m-d'),
-			'DebtorName' => $this->debtorName,
-			'DebtorAccountIBAN' => $this->debtorAccountIBAN,
-			'DebtorAccountCurrency' => $this->debtorAccountCurrency,
-			'DebtorAgentBIC' => $this->debtorAgentBIC,
-			'ChargeBearer' => $this->chargeBearer,
-			'CreditorList' => $this->creditorList,
-		);
-
-		foreach ($params as $f => $v) {
-			if (isset($data[$f]))
-				$params[$f] = $data[$f];
-		}
+		$datetime = new DateTime();
+		$creationDateTime = $datetime->format('Y-m-d\TH:i:s');
+		$requestedExecutionDate = $datetime->format('Y-m-d');
 		
-		$xml = simplexml_load_string('<?xml version="1.0" encoding="UTF-8"?><Document xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="urn:iso:std:iso:20022:tech:xsd:pain.001.001.03"></Document>');
+		$xml = simplexml_load_string(self::INITIAL_STRING);
 
 		$GrpHdr = $xml->addChild('CstmrCdtTrfInitn')->addChild('GrpHdr');
 		$GrpHdr->addChild('MsgId', $this->messageIdentification);
-		$GrpHdr->addChild('CreDtTm', $params['CreationDateTime']);
+		$GrpHdr->addChild('CreDtTm', $creationDateTime);
 		$GrpHdr->addChild('NbOfTxs', $this->numberOfTransactions);
-		$GrpHdr->addChild('CtrlSum', $params['HeaderControlSum']);
-		$GrpHdr->addChild('InitgPty')->addChild('Nm', $params['InitiatingPartyName']);
-		
+		$GrpHdr->addChild('CtrlSum', $this->headerControlSum);
+		$GrpHdr->addChild('InitgPty')->addChild('Nm', $this->initiatingPartyName);
+
 		$PmtInf = $xml->CstmrCdtTrfInitn->addChild('PmtInf');
-		$PmtInf->addChild('PmtInfId', $params['PaymentInfoId']);
-		$PmtInf->addChild('PmtMtd', $params['PaymentMethod']);
+		$PmtInf->addChild('PmtInfId', $this->paymentInfoId);
+		$PmtInf->addChild('PmtMtd', $this->paymentMethod);
 		$PmtInf->addChild('NbOfTxs', $this->numberOfTransactions);
-		$PmtInf->addChild('CtrlSum', $params['PaymentControlSum']);
-		$PmtInf->addChild('PmtTpInf')->addChild('SvcLvl')->addChild('Cd', $params['PaymentTypeInfoCode']);
-		$PmtInf->addChild('ReqdExctnDt', $params['RequestedExecutionDate']);
-		$PmtInf->addChild('Dbtr')->addChild('Nm', $params['DebtorName']);
-		
+		$PmtInf->addChild('CtrlSum', $this->paymentControlSum);
+		$PmtInf->addChild('PmtTpInf')->addChild('SvcLvl')->addChild('Cd', $this->paymentTypeInfoCode);
+		$PmtInf->addChild('ReqdExctnDt', $requestedExecutionDate);
+		$PmtInf->addChild('Dbtr')->addChild('Nm', $this->debtorName);
+
 		$DbtrAcct = $PmtInf->addChild('DbtrAcct');
-		$DbtrAcct->addChild('Id')->addChild('IBAN', $params['DebtorAccountIBAN']);
-		$DbtrAcct->addChild('Ccy', $params['DebtorAccountCurrency']);
-	
-		$PmtInf->addChild('DbtrAgt')->addChild('FinInstnId')->addChild('BIC', $params['DebtorAgentBIC']);
-		$PmtInf->addChild('ChrgBr', $params['ChargeBearer']);
-		
-		foreach($params['CreditorList'] as $rData) {
+		$DbtrAcct->addChild('Id')->addChild('IBAN', $this->debtorAccountIBAN);
+		$DbtrAcct->addChild('Ccy', $this->debtorAccountCurrency);
+
+		$PmtInf->addChild('DbtrAgt')->addChild('FinInstnId')->addChild('BIC', $this->debtorAgentBIC);
+		$PmtInf->addChild('ChrgBr', $this->chargeBearer);
+
+		foreach($this->creditorList as $creditor) {
 			$CdtTrfTxInf = $PmtInf->addChild('CdtTrfTxInf');
 			$PmtId = $CdtTrfTxInf->addChild('PmtId');
-			$PmtId->addChild('InstrId', $rData['CreditorPaymentId']);
-			$PmtId->addChild('EndToEndId', $rData['CreditorPaymentEndToEndId']);
-			$CdtTrfTxInf->addChild('Amt')->addChild('InstdAmt', $rData['CreditorPaymentAmount'])->addAttribute('Ccy', $rData['CreditorPaymentCurrency']);
-			$CdtTrfTxInf->addChild('CdtrAgt')->addChild('FinInstnId')->addChild('BIC', $rData['CreditorBIC']);
-			$CdtTrfTxInf->addChild('Cdtr')->addChild('Nm', $rData['CreditorName']);
-			$CdtTrfTxInf->addChild('CdtrAcct')->addChild('Id')->addChild('IBAN', $rData['CreditorAccountIBAN']);
-			$CdtTrfTxInf->addChild('RmtInf')->addChild('Ustrd', $rData['RemittanceInformation']);
+			$PmtId->addChild('InstrId', $creditor['CreditorPaymentId']);
+			$PmtId->addChild('EndToEndId', $creditor['CreditorPaymentEndToEndId']);
+			$CdtTrfTxInf->addChild('Amt')->addChild('InstdAmt', $creditor['CreditorPaymentAmount'])->addAttribute('Ccy', $creditor['CreditorPaymentCurrency']);
+			$CdtTrfTxInf->addChild('CdtrAgt')->addChild('FinInstnId')->addChild('BIC', $creditor['CreditorBIC']);
+			$CdtTrfTxInf->addChild('Cdtr')->addChild('Nm', $creditor['CreditorName']);
+			$CdtTrfTxInf->addChild('CdtrAcct')->addChild('Id')->addChild('IBAN', $creditor['CreditorAccountIBAN']);
+			$CdtTrfTxInf->addChild('RmtInf')->addChild('Ustrd', $creditor['RemittanceInformation']);
 		}
 
 		return $xml->asXML();
