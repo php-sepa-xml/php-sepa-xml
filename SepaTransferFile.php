13e4b888df232b0b776a2dda0882c50bc913f69b
isevi <ianare@gmail.com>
2012-11-16 10:29:49 +0100

    add license info

--- SepaTransferFile.php
+++ SepaTransferFile.php
@@ -8,6 +8,20 @@
  *
  * @copyright © Digitick <www.digitick.net> 2012
  * @license GNU Lesser General Public License v3.0
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Lesser Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * 
  * @author Jérémy Cambon
  * @author Ianaré Sévi
  * @author Vincent MOMIN
@@ -31,7 +45,7 @@ class SepaTransferFile
 	 */
 	public $debtorName;
 	/**
-	 * @var string Debtor's account IBAN.
+	 * @var string Debtor's account IBAN. 
 	 */
 	public $debtorAccountIBAN;
 	/**
