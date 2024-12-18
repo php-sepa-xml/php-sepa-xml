1a0c49a322e1541a5301e2d7aa3c820007d3a910
borislavsabev <borislavsabev@gmail.com>
2024-12-18 15:43:43 +0200

    Initial addition of rector/rector and phpstan/phpstan

---
+++ rector.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+    // uncomment to reach your current PHP version
+    // ->withPhpSets()
+    ->withTypeCoverageLevel(0);
